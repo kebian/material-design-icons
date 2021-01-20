9c95145c65bef79fe9f2d6249f5f6592f5933542
Rob Stiles <robs@codexsoftware.co.uk>
2021-01-20 17:22:01 +0000

    fixed esm / commonjs dual package problem

--- tests/mjs.mjs
+++ tests/mjs.mjs
@@ -1,4 +1,4 @@
-import { categories, icons } from '../esm.mjs'
+import { categories, icons } from '../index.mjs'
 const assertArrayWithContent = arr => {
     if (!Array.isArray(arr)) throw "Not an array"
     if (!arr.length) throw "Empty array"
